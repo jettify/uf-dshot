35cae8dcea3716cfc8d9f9c08c8e6e94d69efc18
Nikolay Novik <nickolainovik@gmail.com>
2026-03-15 16:22:01 -0400

    docs: Add simple demo of DShot300 and STM32

---
+++ examples/dshot-simple-stm32/src/main.rs
@@ -0,0 +1,37 @@
+#![no_std]
+#![no_main]
+
+#[macro_use]
+mod fmt;
+
+use embassy_time::{Duration, Ticker};
+
+#[cfg(not(feature = "defmt"))]
+use panic_halt as _;
+
+use uf_dshot::embassy_stm32::{DshotTxPin, Stm32DshotController};
+use uf_dshot::DshotSpeed;
+#[cfg(feature = "defmt")]
+use {defmt_rtt as _, panic_probe as _};
+
+const ESC_SPEED: DshotSpeed = DshotSpeed::Dshot300;
+const DEMO_THROTTLE: u16 = 200;
+
+#[embassy_executor::main]
+async fn main(_spawner: embassy_executor::Spawner) {
+    let p = embassy_stm32::init(Default::default());
+
+    let tx_pin = DshotTxPin::new_ch1(p.PA8);
+    let mut esc = Stm32DshotController::tx_only(p.TIM1, tx_pin, p.DMA2_CH5, ESC_SPEED);
+    let tx_period_us = u64::from(ESC_SPEED.timing_hints().min_frame_period_us);
+
+    info!("arming");
+    unwrap!(esc.arm().await);
+
+    let mut ticker = Ticker::every(Duration::from_micros(tx_period_us));
+    info!("spinning at throttle {}", DEMO_THROTTLE);
+    loop {
+        unwrap!(esc.send_throttle(DEMO_THROTTLE).await);
+        ticker.next().await;
+    }
+}
